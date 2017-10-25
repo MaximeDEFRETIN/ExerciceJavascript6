e58fd49b08f15ed36c5c1c3f79125e8921a68980
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-10-25 09:30:49 +0200

    Exercice 6 : Javascript

---
+++ Javascript.js
@@ -0,0 +1,6 @@
+function form1() {
+  var premier_nombre = document.getElementById('premier_nombre').value;
+  var deuxieme_nombre = document.getElementById('deuxieme_nombre').value;
+
+  alert(premier_nombre % deuxieme_nombre);
+}
