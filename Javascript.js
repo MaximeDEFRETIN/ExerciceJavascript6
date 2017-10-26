e7cb9a5e09ab9ed6a28422d4c30af8d7e330a813
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-10-26 09:23:11 +0200

    Modif

--- Javascript.js
+++ Javascript.js
@@ -1,6 +1,10 @@
 function form1() {
   var premier_nombre = document.getElementById('premier_nombre').value;
   var deuxieme_nombre = document.getElementById('deuxieme_nombre').value;
-
-  alert(premier_nombre % deuxieme_nombre);
+  if(isNaN(premier_nombre) == false && isNaN(deuxieme_nombre) == false && deuxieme_nombre != 0){
+    alert(premier_nombre % deuxieme_nombre);
+  }
+  else{
+    alert("Saisissez un nombre différent de 0 !")
+  }
 }
